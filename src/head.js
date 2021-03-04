4dd26e6b2bf366f50b1c238d77fca45972703f45
Md Mubasir <54818531+mdmub0587@users.noreply.github.com>
2021-03-05 00:10:29 +0530

    implemented and tested head funtion

---
+++ src/head.js
@@ -0,0 +1,5 @@
+const head = (list) => {
+    if(!list.length) return null;
+    else return list[0];
+}
+module.exports = head
---
+++ src/head.spec.js
@@ -0,0 +1,11 @@
+const head = require('./head')
+
+describe('Head', () => {
+
+    it('Head of [1,2,3] should be 1', () => {
+        expect(head([1,2,3])).toEqual(1);
+    })
+    it('Head of [] should be null or undefined', () => {
+        expect(head([])).toBeNull();
+    })
+})
